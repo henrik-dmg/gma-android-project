6baa71838b837055dab51ab60a1949b0d4753299
Henrik Panhans <henrik@panhans.dev>
2022-05-03 14:41:36 +0200

    Initial commit

---
+++ app/src/main/java/htw/gma_sose22/metronompro/ui/home/HomeViewModel.java
@@ -0,0 +1,20 @@
+package htw.gma_sose22.metronompro.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+}
