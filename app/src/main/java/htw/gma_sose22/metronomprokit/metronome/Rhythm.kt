d734b5b7c6d6e9f06fc1861504a32f9e0d7f139d
Henrik Panhans <henrik@panhans.dev>
2022-05-10 17:38:49 +0200

    More stolen code

---
+++ app/src/main/java/htw/gma_sose22/metronomprokit/metronome/Rhythm.kt
@@ -0,0 +1,15 @@
+package htw.gma_sose22.metronomprokit.metronome
+
+enum class Rhythm(val value: Int) {
+    QUARTER(1),
+    EIGHTH(2),
+    SIXTEENTH(4);
+
+    companion object {
+        private val values = values()
+    }
+
+    fun next(): Rhythm {
+        return values()[(this.ordinal + 1) % values.size]
+    }
+}
