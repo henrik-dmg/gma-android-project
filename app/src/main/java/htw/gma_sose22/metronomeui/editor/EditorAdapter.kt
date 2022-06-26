21f8b78b09de0a107dbd1f5affd8b2f60d333b91
Henrik Panhans <henrik@panhans.dev>
2022-06-26 14:40:00 +0200

    Using Binding in ViewHolder
    Signed-off-by: Henrik Panhans <henrik@panhans.dev>

--- app/src/main/java/htw/gma_sose22/metronomeui/editor/EditorAdapter.kt
+++ app/src/main/java/htw/gma_sose22/metronomeui/editor/EditorAdapter.kt
@@ -1,17 +1,21 @@
 package htw.gma_sose22.metronomeui.editor
 
+import android.util.Log
 import android.view.*
 import android.widget.Button
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import htw.gma_sose22.R
+import htw.gma_sose22.databinding.EditorlistItemBinding
 import htw.gma_sose22.metronomekit.beat.Beat
+import htw.gma_sose22.metronomekit.beat.Tone
 
 class EditorAdapter: ListAdapter<Beat, EditorAdapter.ViewHolder>(BeatDiffCallback) {
 
-   inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-      private val incrementNotesButton: Button = itemView.findViewById(R.id.increment_notes_button)
-      private val decrementNotesButton: Button = itemView.findViewById(R.id.decrement_notes_button)
+   inner class ViewHolder(private val binding: EditorlistItemBinding) : RecyclerView.ViewHolder(binding.root) {
+      private val incrementNotesButton: Button = binding.beatView.tonesView.incrementNotesButton
+      private val decrementNotesButton: Button = binding.beatView.tonesView.decrementNotesButton
       private var currentBeat: Beat? = null
 
       init {
@@ -19,21 +23,60 @@ class EditorAdapter: ListAdapter<Beat, EditorAdapter.ViewHolder>(BeatDiffCallbac
 
       fun bind(beat: Beat) {
          currentBeat = beat
+         incrementNotesButton.setOnClickListener {
+            currentBeat?.let {
+               if (it.addNote()) {
+                  updateBeatView(it)
+               }
+            }
+         }
+         decrementNotesButton.setOnClickListener {
+            currentBeat?.let {
+               if (it.removeNote()) {
+                  updateBeatView(it)
+               }
+            }
+         }
+
       }
 
       fun unbind() {
          currentBeat = null
       }
+
+      private fun updateBeatView(beat: Beat) {
+         val beatButtons = binding.beatView.tonesView.beatButtons
+         val currentNumberOfButtons = beatButtons.childCount
+
+         val tones = beat.makeNotes()
+
+         for (i in 0 until currentNumberOfButtons) {
+            val button = beatButtons.getChildAt(i)
+            if (i < tones.size) {
+               button.visibility = View.VISIBLE
+               updateButtonImage(button as MaterialButton, tones[i])
+            } else {
+               button.visibility = View.GONE
+            }
+         }
+
+         context
+         binding.beatView.tonesView.noteCountLabel.text = resources.getQuantityString(R.plurals.notes_count, beat.noteCount, beat.noteCount)
+      }
+
+      private fun updateButtonImage(button: MaterialButton, tone: Tone) {
+         when (tone) {
+            Tone.emphasised -> button.icon = resources.getDrawable(R.drawable.ic_note_emphasised)
+            Tone.muted -> button.icon = resources.getDrawable(R.drawable.ic_note_muted)
+            Tone.regular -> button.icon = resources.getDrawable(R.drawable.ic_note_default)
+         }
+      }
    }
 
    /* Creates and inflates view and return FlowerViewHolder. */
    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-      val context = parent.context
-      val inflater = LayoutInflater.from(context)
-      // Inflate the custom layout
-      val editorListItem = inflater.inflate(R.layout.editorlist_item, parent, false)
-      // Return a new holder instance
-      return ViewHolder(editorListItem)
+      val binding = EditorlistItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+      return ViewHolder(binding)
    }
 
    /* Gets current flower and uses it to bind view. */
